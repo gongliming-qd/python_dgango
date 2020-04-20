f4f618690582bd3b5561308191fa7ed001b5ec9a
glm <13145846564@163.com>
2020-04-20 23:07:21 +0800

    第一次

---
+++ polls/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('liming/', views.static, name='static'),
+    path('signing/', views.signings, name='static'),
+]
---
+++ polls/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.db import connection
+from django.core import signing
+
+# 加密解密算法
+
+
+def index(request):
+    print(request.GET.get('name'))
+    cursor = connection.cursor()
+    cursor.execute("select * from member")
+    rows = cursor.fetchall()
+    rows = list(rows)
+    result = []
+    for key in rows:
+        list_value = list(key)
+        result.append(list_value)
+    print(result)
+    return HttpResponse(result)
+
+
+def static(request):
+    return render(request, 'index.html')
+
+
+def signings(request):
+    admin = request.GET.get('admin')
+    psw = request.GET.get('psw')
+    person = {'admin': admin, 'psw': psw}
+    person_change = signing.dumps(person)
+    print(person_change)
+    person_cancel = signing.loads(person_change)
+    print(person_cancel)
+
+    return HttpResponse('HELLO')
