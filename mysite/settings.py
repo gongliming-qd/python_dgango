3c1aaa1d9d3f32830a418fd7b5ca2582bfd41b22
v_lmgong <v_lmgong@tencent.com>
2020-04-21 11:31:01 +0800

    xxx

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#fux6x8cvkxhlquy1o%j(0x-o^gjtgt5ew&8u+eolo8k$vig9_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['139.199.81.62']
 
 
 # Application definition
