f7002db81b528fd54a4b95d464b2808a7fdc4922
v_lmgong <v_lmgong@tencent.com>
2020-04-22 16:48:02 +0800

    NIHAO

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#fux6x8cvkxhlquy1o%j(0x-o^gjtgt5ew&8u+eolo8k$vig9_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['139.199.81.62']
 
@@ -123,7 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'templates')
 STATICFILES_DIRS = [
   os.path.join(BASE_DIR, "templates")
 ]
+
 
