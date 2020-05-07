43a56163c78a90afd6de31a4d955c70c98a457c3
v_lmgong <v_lmgong@tencent.com>
2020-05-07 10:26:24 +0800

    萨达

--- polls/urls.py
+++ polls/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('liming/', views.index, name='index'),
     path('gong/', views.static, name='static'),
+    path('index/', views.index, name='index'),
+    path('api/liming/', views.index, name='index'),
     path('signing/', views.signings, name='signings'),
     # path('gong/', views.static1, name='static1'),
 ]
